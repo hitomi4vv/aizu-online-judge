54b4bd5f0f5ae4855b0ffaef2756b6c789ecc174
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-01-21 16:57:04 +0900

    Add any source code files of Print Test Cases

---
+++ introduction/ITP1_3_B-Print Test Cases.py
@@ -0,0 +1,6 @@
+count = 1
+line = input()
+while line != '0':
+  print('Case ' + str(count) + ': ' +line)
+  count += 1
+  line = input()
---
+++ introduction/ITP1_3_B-Print Test Cases.rb
@@ -0,0 +1,5 @@
+count = 1
+while (line = gets.chomp) != "0"
+  puts "Case #{count}: #{line}"
+  count += 1
+end
