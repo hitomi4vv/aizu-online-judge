4f8bd811c148fb83790d74116f73e7c6b504cc46
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-02-20 00:19:26 +0900

    Add .c source code file of Print a Rectangle

---
+++ introduction/ITP1_5_A-Print a Rectangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int h, w, i, j;
+  while(1) {
+    scanf("%d %d", &h, &w);
+    if(h + w == 0) break;
+    for(i = 0; i < h; i++) {
+      for(j = 0; j < w; j++) {
+        printf("#");
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+  return 0;
+}
