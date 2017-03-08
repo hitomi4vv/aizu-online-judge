a62e30344a2204c3cf3dc38791aa50d57f358974
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-03-09 01:41:09 +0900

    Add .cpp source code file of Print a Chessboard

---
+++ introduction/ITP1_5_C-Print a Chessboard.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int h, w;
+  while (cin >> h >> w, h + w != 0) {
+    for(int i = 0; i < h; i++) {
+      for(int j = 0; j < w; j++) cout << (!((i%2 + j%2)%2)? '#':'.');
+      cout << endl;
+    }
+    cout << endl;
+  }
+  return 0;
+}
