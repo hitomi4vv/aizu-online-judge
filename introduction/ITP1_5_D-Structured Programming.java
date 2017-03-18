1683383a1d7bfa20575ec7e260d3c318bd9218a6
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-03-18 17:05:47 +0900

    aw.... wrong

--- introduction/ITP1_5_D-Structured Programming.java
+++ introduction/ITP1_5_D-Structured Programming.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     int n = new Scanner(System.in).nextInt();
-    String str = "";
-    for(int i = 3; i <= new Scanner(System.in).nextInt(); i++)
-      if(i%3 == 0 || Integer.toString(i).indexOf("3") >= 0) str += " " + i;
-    System.out.println(str);
+    for(int i = 3; i <= n; i++)
+      if(i%3 == 0 || Integer.toString(i).indexOf("3") >= 0) System.out.print(" " + i);
+    System.out.println();
   }
 }
