c2cebb5dd59c43683a63af926405eb515822d31d
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-04-12 01:05:34 +0900

    done

--- introduction/ITP1_6_C-Official House.scala
+++ introduction/ITP1_6_C-Official House.scala
@@ -4,6 +4,5 @@ object Main extends App {
   println(Source.stdin.getLines.toList.tail
     .map(_.split(" ").map(_.toInt))
     .foldLeft(Array.fill(4, 3, 10)(0)){ case (arr, Array(b, f, r, v)) => arr(b-1)(f-1)(r-1) += v; arr }
-    .map(_ + "\n")
-    .mkString("#" * 20 + "\n"))
+    .map(_.map(_.map(" " + _).mkString("")).mkString("\n")).mkString("\n" + "#" * 20 + "\n"))
 }
