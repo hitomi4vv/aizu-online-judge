e2718dcfc0e38fa730d09e27b2d4b5fb20a68926
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-04-17 23:01:52 +0900

    Add .js source code file of Matrix Vector Multiplication

---
+++ introduction/ITP1_6_D-Matrix Vector Multiplication.js
@@ -0,0 +1,14 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(chunk) {
+  var lines = chunk.split('\n');
+  var arr = lines[0].split(' ').map(Number);
+  var n = arr[0], m = arr[1];
+  lines.shift();
+  var a = lines.splice(0, n), b = lines.splice(0, m);
+  for(var i = 0; i < n; i++) {
+    var sum = 0;
+    for(var j = 0; j < m; j++) sum += a[i].split(' ')[j] * b[j];
+    console.log(sum);
+  }
+});
