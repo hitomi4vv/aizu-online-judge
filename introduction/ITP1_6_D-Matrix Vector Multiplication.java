1d5a5fc76377d349a011678e8428b78dd2a67eb8
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-04-20 14:36:31 +0900

    optimize the source code

--- introduction/ITP1_6_D-Matrix Vector Multiplication.java
+++ introduction/ITP1_6_D-Matrix Vector Multiplication.java
@@ -4,13 +4,11 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt(), m = sc.nextInt();
-    int[][] a = new int[n][m];
-    int[] b = new int[m];
-    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) a[i][j] = sc.nextInt();
-    for(int j = 0; j < m; j++) b[j] = sc.nextInt();
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    while(sc.hasNextInt()) arr.add(sc.nextInt());
     for(int i = 0; i < n; i++) {
       int sum = 0;
-      for(int j = 0; j < m; j++) sum += a[i][j] * b[j];
+      for(int j = 0; j < m; j++) sum += arr.get(i*m+j) * arr.get(n*m+j);
       System.out.println(sum);
     }
   }
