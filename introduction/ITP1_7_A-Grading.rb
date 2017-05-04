d5310e34bc74c613c42b0f9b65c833a89d2f644a
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-05-05 00:09:00 +0900

    Add .rb source code file of Grading

---
+++ introduction/ITP1_7_A-Grading.rb
@@ -0,0 +1,13 @@
+while(m, f, r = gets.split.map(&:to_i)) != [-1, -1, -1]
+  if m == -1 || f == -1 || (m += f) < 30
+    puts 'F'
+  elsif m >= 80
+    puts 'A'
+  elsif m >= 65
+    puts 'B'
+  elsif m >= 50 || r >= 50
+    puts 'C'
+  else
+    puts 'D'
+  end
+end
