ef5767cd19e9efd69455164934087682f9499b2e
Hitomi <hitomi4vv@outlook.jp>
2017-05-17 22:57:03 +0900

    hmm....

--- introduction/ITP1_7_C-Spreadsheet.c
+++ introduction/ITP1_7_C-Spreadsheet.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-
 int main() {
-  int r, c, v, i, j;
+  int r, c, v, i, j, rsum[101]={0}, csum[101]={0};
   scanf("%d %d", &r, &c);
-  int rsum, csum[101] = {};
-  for(i = 0; i < r; i++) {
-    rsum = 0;
-    for(j = 0; j < c; j++) {
-      scanf("%d", &v);
-      printf("%d ", v);
-      rsum += v;
-      csum[j] += v;
+  for(i = 0; i <= r; i++)
+    for(j = 0; j <= c; j++) {
+      if(i < r && j < c) {
+        scanf("%d", &v);
+        rsum[i] += v;
+        csum[j] += v, csum[c] += v;
+      }
+      printf("%d%s", i==r?csum[j]:j==c?rsum[i]:v, j==c?"\n":" ");
     }
-    printf("%d\n", rsum);
-  }
-  rsum = 0;
-  for(j = 0; j < c; j++) {
-    rsum += csum[j];
-    printf("%d ", csum[j]);
-  }
-  printf("%d\n", rsum);
   return 0;
 }
