e7cacd076c043263f36652d8c935cc32d5db3527
Hitomi <hitomi4vv@outlook.jp>
2017-05-21 21:24:12 +0900

    For some reason, Judge was 'Wrong Answer'...why?...

---
+++ introduction/ITP1_7_C-Spreadsheet.js
@@ -0,0 +1,14 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(chunk) {
+  var sum = [];
+  var lines = chunk.split('\n');
+  lines.shift();
+  lines.some(function(line) {
+    var arr = line.split(' ').map(Number);
+    arr.push(arr.reduce(function(a, b){return a+b;}, 0));
+    sum.push(arr);
+    console.log(arr.join(' '));
+  });
+  console.log(sum.reduce(function(a, b){return a.map(function(v, i){return v+b[i];});}).join(' '));
+});
