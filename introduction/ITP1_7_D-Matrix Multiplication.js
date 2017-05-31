91999a3ba7741b5d73eb5cddf510a821a26df815
Hitomi <hitomi4vv@outlook.jp>
2017-05-31 19:36:13 +0900

    Add .js source code file of Matrix Multiplication

--- introduction/ITP1_7_D-Matrix Multiplication.js
+++ introduction/ITP1_7_D-Matrix Multiplication.js
@@ -1,16 +1,19 @@
+var lines = '';
+
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-process.stdin.on('data', function(chunk) {
-  var b = chunk.split('\n').map(function(s){return s.split(' ').map(Number);});
+process.stdin.on('data', function(chunk) {lines += chunk;});
+process.stdin.on('end', function() {
+  var b = lines.trim().split('\n').map(function(s){return s.split(' ').map(Number);});
   var n = b[0][0], m = b[0][1], l = b[0][2];
   b.shift();
   var a = b.splice(0, n);
-  var s = '';
+  var c = '';
   for(var i = 0; i < n; i++)
     for(var j = 0; j < l; j++) {
-      var c = 0;
-      for(var k = 0; k < m; k++) c += a[i][k] * b[k][j];
-      s += c + (j+1 == l? '\n':' ');
+      var s = 0;
+      for(var k = 0; k < m; k++) s += a[i][k] * b[k][j];
+      c += s + (j+1 == l? '\n':' ');
     }
-  console.log(s.trim());
+  console.log(c.trim());
 });
