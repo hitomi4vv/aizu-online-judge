ceb4cf643e913df57c6675688fa02e9f04bb4bc8
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-06-01 13:08:27 +0900

    wip

---
+++ introduction/ITP1_7_D-Matrix Multiplication.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt(), m = sc.nextInt(), l = sc.nextInt();
+    int[][] a = new int[n][m];
+    int[][] b = new int[m][l];
+  }
+}
