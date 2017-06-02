0a8359720990dfa41be2931e173c2777f819ec08
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-06-02 18:23:53 +0900

    uh

--- introduction/ITP1_7_D-Matrix Multiplication.java
+++ introduction/ITP1_7_D-Matrix Multiplication.java
@@ -3,7 +3,12 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt(), m = sc.nextInt(), l = sc.nextInt();
-    int[][] a = new int[n][m];
-    int[][] b = new int[m][l];
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    while(sc.hasNextInt()) arr.add(sc.nextInt());
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < l; j++) {
+        System.out.println(i+":"+j);
+      }
+    }
   }
 }
