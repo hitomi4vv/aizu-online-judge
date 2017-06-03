172c4216ae6281dc35b8913fab991e507f8f7542
Hitomi <hitomi4vv@outlook.jp>
2017-06-03 21:45:58 +0900

    Judge was 'Runtime Error' in TestCase17

--- introduction/ITP1_7_D-Matrix Multiplication.java
+++ introduction/ITP1_7_D-Matrix Multiplication.java
@@ -2,13 +2,14 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt(), m = sc.nextInt(), l = sc.nextInt();
-    ArrayList<Integer> arr = new ArrayList<Integer>();
-    while(sc.hasNextInt()) arr.add(sc.nextInt());
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < l; j++) {
-        System.out.println(i+":"+j);
+    int n = sc.nextInt(), m = sc.nextInt(), l = sc.nextInt(), i, j, k;
+    int[][] a = new int[n][m], b = new int[m][l], c = new int[n][l];
+    for(i = 0; i < n; i++) for(j = 0; j < m; j++) a[i][j] = sc.nextInt();
+    for(i = 0; i < m; i++) for(j = 0; j < l; j++) b[i][j] = sc.nextInt();
+    for(i = 0; i < n; i++)
+      for(j = 0; j < l; j++) {
+        for(k = 0; k < m; k++) c[i][j] += a[i][k] * b[k][j];
+        System.out.print(c[i][j] + (j+1 == l? "\n":" "));
       }
-    }
   }
 }
