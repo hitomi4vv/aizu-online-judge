33482a432cf29869b9ea29d5c93363acbe6e77b6
Hitomi <hitomi4vv@outlook.jp>
2017-06-04 13:17:34 +0900

    wip

---
+++ introduction/ITP1_7_D-Matrix Multiplication.cs
@@ -0,0 +1,10 @@
+using System;
+public class Program {
+  public static void Main() {
+    var line = Console.ReadLine().Split();
+    int n = int.Parse(line[0]), m = int.Parse(line[1]), l = int.Parse(line[1]), i, j, k;
+    var a = new long[n, m];
+    var b = new long[m, l];
+    var c = new long[n, l];
+  }
+}
