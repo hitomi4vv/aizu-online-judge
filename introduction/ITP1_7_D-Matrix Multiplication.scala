84ae72dc3c41a28f554831c969d807ef27790491
Hitomi <hitomi4vv@outlook.jp>
2017-06-10 14:06:53 +0900

    maybe it's enough

--- introduction/ITP1_7_D-Matrix Multiplication.scala
+++ introduction/ITP1_7_D-Matrix Multiplication.scala
@@ -1,13 +1,14 @@
 import scala.collection.mutable.ArrayBuffer
 object Main extends App {
   val Array(n, m, l) = readLine.split(" ").map(_.toInt)
-  var arr = ArrayBuffer.empty[Array[Int]]
-  for(i <- 0 until n) arr += readLine.split(" ").map(_.toInt)
-  val a = arr.toArray
-  arr = ArrayBuffer.empty[Array[Int]]
-  for(i <- 0 until m) arr += readLine.split(" ").map(_.toInt)
-  val b = arr.toArray
-  val c = for(i <- 0 until n) yield for(j<- 0 until l)
+  val matrix = (ii: Int) => {
+    val arr = ArrayBuffer.empty[Array[Int]]
+    for(i <- 0 until ii) arr += readLine.split(" ").map(_.toInt)
+    arr.toArray
+  }
+  val a = matrix(n)
+  val b = matrix(m)
+  val c = for(i <- 0 until n) yield for(j <- 0 until l)
     yield (0 until m).foldLeft(0L) {(sum,k) => sum + a(i)(k) * b(k)(j)}
   c.foreach(row => println(row.mkString(" ")))
 }
