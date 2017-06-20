ee6cb92a877a929829d1fcb3ba31806820d90a13
Hitomi <hitomi4vv@outlook.jp>
2017-06-20 23:59:17 +0900

    wip

---
+++ introduction/ITP1_8_B-Sum of Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String line;
+    while(!(line = sc.nextLine()).equals("0")) {
+      System.out.println(line);
+    }
+  }
+}
