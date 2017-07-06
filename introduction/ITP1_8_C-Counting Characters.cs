3f9a8f59b701e40c2e84a5322628c02a640c788e
Hitomi <hitomi4vv@outlook.jp>
2017-07-06 21:30:27 +0900

    Add .cs source code file of Counting Characters

--- introduction/ITP1_8_C-Counting Characters.cs
+++ introduction/ITP1_8_C-Counting Characters.cs
@@ -3,9 +3,9 @@ using System.Linq;
 public class Program {
   public static void Main() {
     string line;
-    var s = new int[26];
-    while ((line = Console.ReadLine()) != null) {
-      line.ToLower().ToCharArray().Select(c => s[c-'a']++);
-    }
+    int[] s = new int[26];
+    while ((line = Console.ReadLine()) != null)
+      foreach(int i in line.ToLower().ToCharArray().Select(c => c-'a').Where(c => c>-1).ToArray()) s[i]++;
+    for(int i = 0; i < 26; i++) Console.WriteLine((char)('a'+i) + " : " + s[i]);
   }
 }
