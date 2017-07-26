441b41dd4cbfb8e3b3a13a7fce51e16384211fa6
Hitomi <hitomi4vv@outlook.jp>
2017-07-26 20:39:57 +0900

    Add .c source code file of Shuffle

--- introduction/ITP1_9_B-Shuffle.c
+++ introduction/ITP1_9_B-Shuffle.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 int main() {
-  char s[201];
+  char s[201], b[201];
   int i, n, h;
   while(scanf("%s", s), strcmp(s, "-")) {
     scanf("%d", &n);
     for(i = 0; i < n; i++) {
       scanf("%d", &h);
-      //TODO: wip
-      printf("%d\n", h);
+      strcpy(b, s+h);
+      strncat(b, s, h);
+      strcpy(s, b);
     }
     printf("%s\n", s);
   }
