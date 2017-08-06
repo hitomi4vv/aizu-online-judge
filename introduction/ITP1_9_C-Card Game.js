169e34f2efe913af2085886c9cd660ed7604f548
Hitomi <hitomi4vv@outlook.jp>
2017-08-06 12:05:39 +0900

    Add .js source code file of Card Game

--- introduction/ITP1_9_C-Card Game.js
+++ introduction/ITP1_9_C-Card Game.js
@@ -1,6 +1,7 @@
-var t, h = 0;
+var t=0, h=0;
 var arr = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 for(var i = 1; i <= arr[0]; i++) {
-  var s = arr[i].split('\n');
-  console.log(s);
+  var s = arr[i].split(' ');
+  if(s[0]==s[1]){t++;h++} else if(s[0]>s[1]) t+=3; else if(s[0]<s[1]) h+=3;
 }
+console.log(t+' '+h);
