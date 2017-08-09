1a6797c5f093e45eb2d4491296fac4ab6fd013cb
Hitomi <hitomi4vv@outlook.jp>
2017-08-09 23:21:00 +0900

    Add .cs source code file of Card Game

---
+++ introduction/ITP1_9_C-Card Game.cs
@@ -0,0 +1,11 @@
+using System;
+public class Program {
+  public static void Main() {
+    int t=0, h=0, n=int.Parse(Console.ReadLine());
+    for(int i = 0; i < n; i++) {
+      var s = Console.ReadLine().Split();
+
+    }
+    Console.WriteLine(t, h);
+  }
+}
