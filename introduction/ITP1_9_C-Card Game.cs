6794d8a69a8003907d38f29bb385d25d5153dfc6
Hitomi <hitomi4vv@outlook.jp>
2017-08-10 23:37:32 +0900

    I made a mistake

--- introduction/ITP1_9_C-Card Game.cs
+++ introduction/ITP1_9_C-Card Game.cs
@@ -3,9 +3,10 @@ public class Program {
   public static void Main() {
     int t=0, h=0, n=int.Parse(Console.ReadLine());
     for(int i = 0; i < n; i++) {
-      var s = Console.ReadLine().Split();
-
+      var arr = Console.ReadLine().Split();
+      var s = string.Compare(arr[0], arr[1]);
+      if(s == 0){t++;h++;}else if(s > 0) t+=3; else if(s < 0) h+=3;
     }
-    Console.WriteLine(t, h);
+    Console.WriteLine(t+" "+h);
   }
 }
