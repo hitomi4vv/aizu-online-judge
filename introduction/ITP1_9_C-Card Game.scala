ddedd9eafa89dceff43e71eee892d166d9a07c9d
Hitomi <hitomi4vv@outlook.jp>
2017-08-12 16:54:48 +0900

    Add .scala source code file of Card Game

--- introduction/ITP1_9_C-Card Game.scala
+++ introduction/ITP1_9_C-Card Game.scala
@@ -1,3 +1,5 @@
 object Main extends App {
-  println(io.Source.stdin.getLines.toList.tail)
+  var t,h=0
+  io.Source.stdin.getLines.toList.tail.map(_.split(" ")).foreach{s => if(s(0) == s(1)){t+=1;h+=1;} else if(s(0) > s(1)) t+=3 else if(s(0) < s(1)) h+=3}
+  println(t+" "+h)
 }
