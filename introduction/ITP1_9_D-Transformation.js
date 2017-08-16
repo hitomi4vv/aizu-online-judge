2ae46825105c5f63991f4d27d5db2937f4203717
Hitomi <hitomi4vv@outlook.jp>
2017-08-16 23:39:20 +0900

    no time

--- introduction/ITP1_9_D-Transformation.js
+++ introduction/ITP1_9_D-Transformation.js
@@ -1,5 +1,14 @@
 var s = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 for(var i = 0; i < s[1]; i++) {
   var o = s[i+2].split(' ');
-  console.log(o);
+  console.log(s[0]);
+  switch(o[0]) {
+    case 'print':
+      console.log(s[0].slice(o[1], o[2]+1));
+      break;
+    case 'reverse':
+        break;
+    case 'replace':
+      break;
+  }
 }
