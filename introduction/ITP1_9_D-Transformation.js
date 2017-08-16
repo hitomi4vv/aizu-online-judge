299b9e005655375ef874dd81a0241801f40ceab5
Hitomi <hitomi4vv@outlook.jp>
2017-08-17 00:02:41 +0900

    Add .js source code file of Transformation

--- introduction/ITP1_9_D-Transformation.js
+++ introduction/ITP1_9_D-Transformation.js
@@ -1,14 +1,16 @@
 var s = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 for(var i = 0; i < s[1]; i++) {
   var o = s[i+2].split(' ');
-  console.log(s[0]);
+  var a = parseInt(o[1]), b = parseInt(o[2])+1;
   switch(o[0]) {
     case 'print':
-      console.log(s[0].slice(o[1], o[2]+1));
+      console.log(s[0].slice(a, b));
       break;
     case 'reverse':
-        break;
+      s[0] = s[0].slice(0, a)+s[0].slice(a, b).split('').reverse().join('')+s[0].slice(b);
+      break;
     case 'replace':
+      s[0] = s[0].slice(0, a)+o[3]+s[0].slice(b);
       break;
   }
 }
