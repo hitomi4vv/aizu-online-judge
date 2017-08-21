e22c6ae6001bc414887397c5c0ef2f3acba59ccd
Hitomi <hitomi4vv@outlook.jp>
2017-08-21 23:12:10 +0900

    Judge was 'Compile Error'

---
+++ introduction/ITP1_9_D-Transformation.scala
@@ -0,0 +1,15 @@
+object Main extends App {
+  val line = io.Source.stdin.getLines.toList
+  var s = line(0)
+  line.drop(2).map(_.split(" ").toList).map(o => {
+    val a = o(1).toInt
+    val b = o(2).toInt+1
+    val as = s slice(0, a)
+    val bs = s slice(b, s size)
+    o(0) match {
+      case "print" => println(s slice(a,b))
+      case "reverse" => s = as+(s slice(a,b)).reverse+bs
+      case "replace" => s = as+o(3)+bs
+    }
+  })
+}
