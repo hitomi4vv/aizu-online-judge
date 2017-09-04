3e3c0e1c9e35b8a936bf4d4d6f35941aa4ef5c5f
Hitomi <hitomi4vv@outlook.jp>
2017-09-04 20:03:44 +0900

    Add any source code files of Distance

--- introduction/ITP1_10_A-Distance.cpp
+++ introduction/ITP1_10_A-Distance.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 int main() {
   double x1, y1, x2, y2;
   cin >> x1 >> y1 >> x2 >> y2;
-  cout << hypot(x2-x1, y2-y1) << endl;
+  cout << fixed << setprecision(8) << hypot(x2-x1, y2-y1) << endl;
   return 0;
 }
