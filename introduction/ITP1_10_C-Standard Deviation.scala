8b46d25877fdd4aef663e000665df0a39141b479
Hitomi <hitomi4vv@outlook.jp>
2017-09-17 00:00:58 +0900

    Add .scala source code file of Standard Deviation

--- introduction/ITP1_10_C-Standard Deviation.scala
+++ introduction/ITP1_10_C-Standard Deviation.scala
@@ -1,6 +1,10 @@
 object Main extends App {
   val lines = io.Source.stdin.getLines
-  while(lines.next != "0") {
-    val line = lines.next.split(" ").map(_.toDouble)
+  var n = lines.next.toInt
+  while(n != 0) {
+    val s = lines.next.split(" ").map(_.toDouble)
+    val m = s.sum / n
+    println(Math.sqrt(s.foldLeft(0d)((x,y)=>x+(y-m)*(y-m))/n))
+    n = lines.next.toInt
   }
 }
