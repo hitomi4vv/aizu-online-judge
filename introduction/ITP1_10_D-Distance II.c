ce85fb968e9e66857df957029a6cd5105b734867
Hitomi <hitomi4vv@outlook.jp>
2017-09-23 00:02:15 +0900

    wip

--- introduction/ITP1_10_D-Distance II.c
+++ introduction/ITP1_10_D-Distance II.c
@@ -2,9 +2,16 @@
 #include <math.h>
 int main() {
   int n, i;
-  double x[101], y[101];
+  double x[101], y[101], d, d1 = 0, d2 = 0, d3 = 0, max = 0;
   scanf("%d",&n);
   for(i = 0; i < n; i++) scanf("%lf", &x[i]);
   for(i = 0; i < n; i++) scanf("%lf", &y[i]);
+  for(i = 0; i < n; i++) {
+    d = x[i] - y[i];
+    d1 += fabs(d);
+    d2 += pow(fabs(d), 2);
+    d3 += pow(fabs(d), 3);
+  }
+  printf("%f\n%f\n%f\n%f\n", d1, sqrt(d2), cbrt(d3), max);
   return 0;
 }
