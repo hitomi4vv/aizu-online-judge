dfe52e8ac6055e949e40c1814ec531dd490c9f61
Hitomi <hitomi4vv@outlook.jp>
2017-09-22 23:54:33 +0900

    wip

--- introduction/ITP1_10_D-Distance II.c
+++ introduction/ITP1_10_D-Distance II.c
@@ -2,9 +2,9 @@
 #include <math.h>
 int main() {
   int n, i;
+  double x[101], y[101];
   scanf("%d",&n);
-  for(i = 0; i < n; i++) {
-
-  }
+  for(i = 0; i < n; i++) scanf("%lf", &x[i]);
+  for(i = 0; i < n; i++) scanf("%lf", &y[i]);
   return 0;
 }
