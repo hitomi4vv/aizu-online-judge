28d2dc5ed5ed1fb3375bfefc3788a4f9fffddd96
Hitomi <hitomi4vv@outlook.jp>
2017-10-05 23:52:48 +0900

    wip

--- introduction/ITP1_11_A-Dice I.js
+++ introduction/ITP1_11_A-Dice I.js
@@ -1,5 +1,7 @@
 var lines = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 var dice = lines[0].split(' ').map(Number);
-var nsew = lines[1];
-
-console.log(dice, nsew);
+var nsew = lines[1].split('');
+function roll(d) {
+  console.log(d);
+}
+nsew.forEach(roll);
