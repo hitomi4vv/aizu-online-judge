4d1292900926991035e534bb29dccf569dae2b7f
Hitomi <hitomi4vv@outlook.jp>
2017-10-07 04:24:01 +0900

    Add .js source code file of Dice I

--- introduction/ITP1_11_A-Dice I.js
+++ introduction/ITP1_11_A-Dice I.js
@@ -1,12 +1,19 @@
 function Dice(labels) {
   this.labels = labels;
   this.roll = function(dir) {
-    console.log(this, dir);
+    var n;
+    switch(dir) {
+      case 'N': n = [1, 5, 2, 3, 0, 4]; break;
+      case 'S': n = [4, 0, 2, 3, 5, 1]; break;
+      case 'E': n = [3, 1, 0, 5, 4, 2]; break;
+      case 'W': n = [2, 1, 5, 0, 4, 3]; break;
+    }
+    n.forEach(function(v){labels.push(labels[v]);});
+    labels.splice(0, 6);
   };
 }
 
 var lines = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 var dice = new Dice(lines[0].split(' ').map(Number));
-lines[1].split('').forEach(function(e) {
-  dice.roll(e);
-});
+lines[1].split('').forEach(function(v){dice.roll(v);});
+console.log(dice.labels[0]);
