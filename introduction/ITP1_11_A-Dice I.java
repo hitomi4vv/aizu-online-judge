992920a39ca02bc7ebbe3b7d600558d36a836ee7
Hitomi <hitomi4vv@outlook.jp>
2017-10-09 09:29:20 +0900

    Add .java source code file of Dice I

--- introduction/ITP1_11_A-Dice I.java
+++ introduction/ITP1_11_A-Dice I.java
@@ -5,6 +5,7 @@ public class Main {
     Dice dice = new Dice(sc.nextLine().split(" "));
     char[] chars = sc.nextLine().toCharArray();
     for(char c:chars) dice.roll(c);
+    System.out.println(dice.getLabel(0));
   }
 }
 
@@ -14,6 +15,16 @@ class Dice {
     this.labels = labels;
   }
   public void roll(char dir) {
-    System.out.println(dir);
+    int n[] = null;
+    switch(dir) {
+      case 'N': n = new int[]{1, 5, 2, 3, 0, 4}; break;
+      case 'S': n = new int[]{4, 0, 2, 3, 5, 1}; break;
+      case 'E': n = new int[]{3, 1, 0, 5, 4, 2}; break;
+      case 'W': n = new int[]{2, 1, 5, 0, 4, 3}; break;
+    }
+    labels = new String[]{labels[n[0]], labels[n[1]], labels[n[2]], labels[n[3]], labels[n[4]], labels[n[5]]};
+  }
+  public String getLabel(int n) {
+    return labels[n];
   }
 }
