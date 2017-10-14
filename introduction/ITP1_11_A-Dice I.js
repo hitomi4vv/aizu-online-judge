d7af2a364b42ddf20474c0d40dc394087cd55f2a
Hitomi <hitomi4vv@outlook.jp>
2017-10-14 14:30:20 +0900

    wip

--- introduction/ITP1_11_A-Dice I.js
+++ introduction/ITP1_11_A-Dice I.js
@@ -1,16 +1,16 @@
 function Dice(labels) {
   this.labels = labels;
-  this.roll = function(dir) {
-    var n;
-    switch(dir) {
-      case 'N': n = [1, 5, 2, 3, 0, 4]; break;
-      case 'S': n = [4, 0, 2, 3, 5, 1]; break;
-      case 'E': n = [3, 1, 0, 5, 4, 2]; break;
-      case 'W': n = [2, 1, 5, 0, 4, 3]; break;
-    }
-    n.forEach(function(v){labels.push(labels[v]);});
-    labels.splice(0, 6);
-  };
+}
+Dice.prototype.roll = function(dir) {
+  var n;
+  switch(dir) {
+    case 'N': n = [1, 5, 2, 3, 0, 4]; break;
+    case 'S': n = [4, 0, 2, 3, 5, 1]; break;
+    case 'E': n = [3, 1, 0, 5, 4, 2]; break;
+    case 'W': n = [2, 1, 5, 0, 4, 3]; break;
+  }
+  var l = this.labels;
+  this.labels = [l[n[0]],l[n[1]],l[n[2]],l[n[3]],l[n[4]],l[n[5]]];
 }
 
 var lines = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
