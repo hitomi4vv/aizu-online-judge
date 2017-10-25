3821f989dd89afc05d4f217913d924c664f11e6c
Hitomi <hitomi4vv@outlook.jp>
2017-10-25 21:54:14 +0900

    maybe it's enough

--- introduction/ITP1_11_B-Dice II.js
+++ introduction/ITP1_11_B-Dice II.js
@@ -7,7 +7,6 @@ function Dice(labels) {
     if(i==5) continue;
     this.patterns = this.patterns.concat(all(this.labels));
   }
-  console.log(this.patterns);
 }
 Dice.prototype.roll = function(dir) {
   var n;
@@ -20,17 +19,15 @@ Dice.prototype.roll = function(dir) {
   var l = this.labels;
   this.labels = [l[n[0]],l[n[1]],l[n[2]],l[n[3]],l[n[4]],l[n[5]]];
 }
-Dice.prototype.setDirection = function(l) {
-  for(var i = 0; i < 8; i++) {
-    if(this.labels[1] == l[1]) break;
-    this.roll("NW"[i%4/3|0]);
-  }
-  while(this.labels[0] != l[0]) this.roll('W');
-}
 
 var lines = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 var dice = new Dice(lines[0].split(' ').map(Number));
 for(var i = 0; i < lines[1]; i++) {
-  dice.setDirection(lines[i+2].split(' '));
-  console.log(dice.labels[2]);
+  for(var j = 0; j < dice.patterns.length; j++) {
+    var p = dice.patterns[j];
+    if(p.slice(0, 2).join(' ') == lines[i+2]) {
+      console.log(p[2]);
+      break;
+    }
+  }
 }
