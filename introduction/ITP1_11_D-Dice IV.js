0256656084080e156d28c6f0658acddefaa4a0c8
Hitomi <hitomi4vv@outlook.jp>
2017-10-27 23:52:03 +0900

    wip

--- introduction/ITP1_11_D-Dice IV.js
+++ introduction/ITP1_11_D-Dice IV.js
@@ -1,5 +1,12 @@
 function Dice(labels) {
   this.labels = labels;
+  this.patterns = [];
+  var all = function(l) {return [[l[0], l[1], l[2], l[3], l[4], l[5]], [l[0], l[2], l[4], l[1], l[3], l[5]], [l[0], l[4], l[3], l[2], l[1], l[5]], [l[0], l[3], l[1], l[4], l[2], l[5]]];};
+  for(var i = 0; i < 7; i++) {
+    this.roll(i<4?'N':'W');
+    if(i==5) continue;
+    this.patterns = this.patterns.concat(all(this.labels));
+  }
 }
 Dice.prototype.roll = function(dir) {
   var n;
@@ -12,28 +19,14 @@ Dice.prototype.roll = function(dir) {
   var l = this.labels;
   this.labels = [l[n[0]],l[n[1]],l[n[2]],l[n[3]],l[n[4]],l[n[5]]];
 }
-Dice.prototype.setDirection = function(l) {
-  for(var i = 0; i < 8; i++) {
-    if(this.labels[1] == l[1]) break;
-    this.roll("NW"[i%4/3|0]);
-  }
-  while(this.labels[0] != l[0]) this.roll('W');
-  if(l.length < 3) return;
-  for(i = 0; i < 8; i++) {
-    if(this.labels[3] == l[3]) break;
-    this.roll("NW"[i%4/3|0]);
-  }
-}
 
 var lines = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
-var n = lines[0];
-var dices = [];
-for(var i = 0; i < n; i++) dices.push(new Dice(lines[i+1].split(' ').map(Number)));
-for(var i = 0; i < n; i++) {
-  for(var j = 0; j < n; j++) {
-    if(i == j) continue;
-    dices[i].setDirection(dices[j].labels);
-    if(dices[i].labels.toString() != dices[j].labels.toString()) {
+var p = [];
+for(var i = 0; i < lines[0]; i++) p = p.concat(new Dice(lines[i+1].split(' ').map(Number)).patterns);
+for(var i = 0; i < p.length; i++) {
+  for(var j = 0; j < p.length; j++) {
+    if(i==j) continue;
+    if(p[i].toString() == p[j].toString()) {
       console.log('No');
       return;
     }
