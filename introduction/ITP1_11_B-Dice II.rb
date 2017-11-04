b7b7d9839e4fc7a3fad0d29c78e8225ed9facda2
Hitomi <hitomi4vv@outlook.jp>
2017-11-04 23:52:10 +0900

    wip

--- introduction/ITP1_11_B-Dice II.rb
+++ introduction/ITP1_11_B-Dice II.rb
@@ -3,6 +3,7 @@ class Dice
   def initialize(labels)
     @labels = labels
     @patterns = []
+    @patterns.concat(all(@labels))
   end
 
   def roll(dir)
@@ -19,10 +20,18 @@ class Dice
     @labels[0]
   end
 
+  def all(l)
+    return [[l[0], l[1], l[2], l[3], l[4], l[5]], [l[0], l[2], l[4], l[1], l[3], l[5]], [l[0], l[4], l[3], l[2], l[1], l[5]], [l[0], l[3], l[1], l[4], l[2], l[5]]]
+  end
+
+  def patterns
+    @patterns
+  end
+
 end
 
 dice = Dice.new(gets.split.map(&:to_i))
+puts dice.patterns
 gets.chomp.to_i.times do
   t, f = gets.chomp.split.map(&:to_i)
-  puts t f;
 end
