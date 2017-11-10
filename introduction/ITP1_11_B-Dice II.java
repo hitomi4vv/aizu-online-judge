237e1993e1bcf697847ad33a10f104de9dda636a
Hitomi <hitomi4vv@outlook.jp>
2017-11-10 22:38:29 +0900

    wip

---
+++ introduction/ITP1_11_B-Dice II.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Dice {
+  private String[] labels = new String[6];
+  private int[][] patterns = {};
+  Dice(String[] labels) {
+    this.labels = labels;
+    for(int i = 0; i < 7; i++) {
+      roll(i<4?'N':'W');
+      if(i==5) continue;
+    }
+  }
+  public void roll(char dir) {
+    int n[] = null;
+    switch(dir) {
+      case 'N': n = new int[]{1, 5, 2, 3, 0, 4}; break;
+      case 'S': n = new int[]{4, 0, 2, 3, 5, 1}; break;
+      case 'E': n = new int[]{3, 1, 0, 5, 4, 2}; break;
+      case 'W': n = new int[]{2, 1, 5, 0, 4, 3}; break;
+    }
+    labels = new String[]{labels[n[0]], labels[n[1]], labels[n[2]], labels[n[3]], labels[n[4]], labels[n[5]]};
+  }
+  public String getLabel(int n) {
+    return labels[n];
+  }
+  public int[][] getPatterns() {
+    return patterns;
+  }
+}
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Dice dice = new Dice(sc.nextLine().split(" "));
+    System.out.println(dice.getPatterns());
+  }
+}
