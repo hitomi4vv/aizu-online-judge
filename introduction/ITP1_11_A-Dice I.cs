89872b1636cb7f60995942f48d778a03ecc9b654
Hitomi <hitomi4vv@outlook.jp>
2017-11-27 21:21:53 +0900

    wip

--- introduction/ITP1_11_A-Dice I.cs
+++ introduction/ITP1_11_A-Dice I.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Linq;
 class Dice {
-  public Dice() {
+  public int[] labels;
+  public Dice(int[] l) {
+    labels = l;
   }
 }
 public class Program {
   public static void Main() {
+    Dice dice = new Dice(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
   }
 }
