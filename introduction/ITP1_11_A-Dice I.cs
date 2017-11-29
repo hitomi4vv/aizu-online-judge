8d08623d9fa3fd1fa315f6f10183bed1ee56362c
Hitomi <hitomi4vv@outlook.jp>
2017-11-29 21:47:18 +0900

    Add .cs source code file of Dice I

--- introduction/ITP1_11_A-Dice I.cs
+++ introduction/ITP1_11_A-Dice I.cs
@@ -6,17 +6,20 @@ class Dice {
     labels = l;
   }
   public void roll(char dir) {
+    int[] n = {};
     switch(dir) {
-      case 'N': labels = new int[]{1, 5, 2, 3, 0, 4}; break;
-      case 'S': labels = new int[]{4, 0, 2, 3, 5, 1}; break;
-      case 'E': labels = new int[]{3, 1, 0, 5, 4, 2}; break;
-      case 'W': labels = new int[]{2, 1, 5, 0, 4, 3}; break;
+      case 'N': n = new int[]{1, 5, 2, 3, 0, 4}; break;
+      case 'S': n = new int[]{4, 0, 2, 3, 5, 1}; break;
+      case 'E': n = new int[]{3, 1, 0, 5, 4, 2}; break;
+      case 'W': n = new int[]{2, 1, 5, 0, 4, 3}; break;
     }
+    labels = new int[]{labels[n[0]], labels[n[1]], labels[n[2]], labels[n[3]], labels[n[4]], labels[n[5]]};
   }
 }
 public class Program {
   public static void Main() {
     Dice dice = new Dice(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
     foreach(char c in Console.ReadLine()) dice.roll(c);
+    Console.WriteLine(dice.labels[0]);
   }
 }
