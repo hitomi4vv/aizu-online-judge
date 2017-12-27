5331b24b25010844ce79fb955ab0215b51b6541e
Hitomi <hitomi4vv@outlook.jp>
2017-12-28 04:15:21 +0900

    Add .rs source code file of X Cubic

--- introduction/ITP1_1_B-X Cubic.rs
+++ introduction/ITP1_1_B-X Cubic.rs
@@ -1,7 +1,7 @@
 use std::io;
 fn main() {
   let mut input = String::new();
-  io::stdin().read_line(&mut input);
+  let _ = io::stdin().read_line(&mut input);
   let n = input.trim().parse::<i32>();
   match n {
     Ok(x) => {println!("{0}", i32::pow(x, 3))},
