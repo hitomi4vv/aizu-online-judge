91c85a61c30d23f4d70002c5b5dc85048b42da0e
Hitomi <hitomi4vv@outlook.jp>
2018-01-02 23:41:39 +0900

    wip

--- introduction/ITP1_11_A-Dice I.c
+++ introduction/ITP1_11_A-Dice I.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 typedef struct Dice {
-    int label[6];
+  int label[6];
 } Dice;
 
 int main() {
   Dice dice;
   int i;
-  for(i=0; i<6; i++) {
-    scanf("%d", &dice.label[i]);
-    printf("%d ", dice.label[i]);
-  }
+  for(i=0; i<6; i++) scanf("%d", &dice.label[i]);
   return 0;
 }
