7251f6730a266ca7a064538a0f62173bff8c71cf
Hitomi <hitomi4vv@outlook.jp>
2018-01-06 21:20:39 +0900

    Add .rs source code file of Rectangle

--- introduction/ITP1_1_C-Rectangle.rs
+++ introduction/ITP1_1_C-Rectangle.rs
@@ -1,7 +1,7 @@
 use std::io;
 fn main() {
   let mut input = String::new();
-  let _ = io::stdin().read_line(&mut input);
-  let ab = input.trim();
-  println!("{:?}", ab);
+  io::stdin().read_line(&mut input).ok().expect("error");
+  let ab: Vec<i32> = input.trim().split(" ").map(|s| s.parse().unwrap()).collect();
+  println!("{0} {1}", ab[0]*ab[1], (ab[0]+ab[1])*2);
 }
