69ecfaf6052b545b31e5e125607ba055ea09fa23
Hitomi <hitomi4vv@outlook.jp>
2018-01-07 22:10:59 +0900

    wip

--- introduction/ITP1_11_A-Dice I.c
+++ introduction/ITP1_11_A-Dice I.c
@@ -3,10 +3,17 @@
 typedef struct Dice {
   int label[6];
 } Dice;
+void roll(struct Dice dice);
 
 int main() {
   Dice dice;
   int i;
   for(i=0; i<6; i++) scanf("%d", &dice.label[i]);
+  roll(dice);
   return 0;
+}
+
+void roll(struct Dice dice) {
+  int i;
+  for(i=0; i<6; i++) printf("%d ", dice.label[i]);
 }
