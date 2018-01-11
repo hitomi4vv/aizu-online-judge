283e5fe0b3bf9f751b03b3f9aba2f09559462fcb
Hitomi <hitomi4vv@outlook.jp>
2018-01-11 17:55:41 +0900

    wip

--- introduction/ITP1_11_A-Dice I.c
+++ introduction/ITP1_11_A-Dice I.c
@@ -18,8 +18,39 @@ int main() {
 
 void roll(struct Dice dice, char dir) {
   int n[6], l[6], i;
-  for(i=0; i<6; i++) {
-    l[i] = dice.labels[i];
-    printf("%d\n", l[i]);
+  for(i=0; i<6; i++) l[i] = dice.labels[i];
+  switch(dir) {
+    case 'N':
+      dice.labels[0] = l[1];
+      dice.labels[1] = l[5];
+      dice.labels[2] = l[2];
+      dice.labels[3] = l[3];
+      dice.labels[4] = l[0];
+      dice.labels[5] = l[4];
+      break;
+    case 'S':
+      dice.labels[0] = l[4];
+      dice.labels[1] = l[0];
+      dice.labels[2] = l[2];
+      dice.labels[3] = l[3];
+      dice.labels[4] = l[5];
+      dice.labels[5] = l[1];
+      break;
+    case 'E':
+      dice.labels[0] = l[3];
+      dice.labels[1] = l[1];
+      dice.labels[2] = l[0];
+      dice.labels[3] = l[5];
+      dice.labels[4] = l[4];
+      dice.labels[5] = l[2];
+      break;
+    case 'W':
+      dice.labels[0] = l[2];
+      dice.labels[1] = l[1];
+      dice.labels[2] = l[5];
+      dice.labels[3] = l[0];
+      dice.labels[4] = l[4];
+      dice.labels[5] = l[3];
+      break;
   }
 }
