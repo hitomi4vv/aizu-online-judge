b8db73204795ff331389dc9d0cb85c9d716beba1
Hitomi <hitomi4vv@outlook.jp>
2018-01-12 23:10:09 +0900

    wip

--- introduction/ITP1_11_A-Dice I.c
+++ introduction/ITP1_11_A-Dice I.c
@@ -3,21 +3,8 @@
 typedef struct Dice {
   int labels[6];
 } Dice;
-void roll(struct Dice dice, char dir);
-
-int main() {
-  Dice dice;
-  int i;
-  char dirs[101];
-  for(i=0; i<6; i++) scanf("%d", &dice.labels[i]);
-  scanf("%s", dirs);
-  for(i=0; i<strlen(dirs); i++) roll(dice, dirs[i]);
-  printf("%d", dice.labels[0]);
-  return 0;
-}
-
 void roll(struct Dice dice, char dir) {
-  int n[6], l[6], i;
+int n[6], l[6], i;
   for(i=0; i<6; i++) l[i] = dice.labels[i];
   switch(dir) {
     case 'N':
@@ -53,4 +40,15 @@ void roll(struct Dice dice, char dir) {
       dice.labels[5] = l[3];
       break;
   }
+}
+
+int main() {
+  Dice dice;
+  int i;
+  char dirs[101];
+  for(i=0; i<6; i++) scanf("%d", &dice.labels[i]);
+  scanf("%s", dirs);
+  for(i=0; i<strlen(dirs); i++) roll(dice, dirs[i]);
+  printf("%d", dice.labels[0]);
+  return 0;
 }
