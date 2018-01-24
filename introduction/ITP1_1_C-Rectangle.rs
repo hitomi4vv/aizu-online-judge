ffceb99a623f30fb7c169ea0025ffe12ece12600
Hitomi <hitomi4vv@outlook.jp>
2018-01-25 05:50:18 +0900

    I'm studying Rust

--- introduction/ITP1_1_C-Rectangle.rs
+++ introduction/ITP1_1_C-Rectangle.rs
@@ -1,7 +1,7 @@
 use std::io;
 fn main() {
   let mut input = String::new();
-  io::stdin().read_line(&mut input).ok().expect("error");
-  let ab: Vec<i32> = input.trim().split(" ").map(|s| s.parse().unwrap()).collect();
+  io::stdin().read_line(&mut input).unwrap();
+  let ab:Vec<i32> = input.trim().split(" ").map(|s| s.parse().unwrap()).collect();
   println!("{0} {1}", ab[0]*ab[1], (ab[0]+ab[1])*2);
 }
