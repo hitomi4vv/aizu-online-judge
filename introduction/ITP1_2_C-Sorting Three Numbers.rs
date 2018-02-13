f4d6cdc86ede430d3426d57dba2a5a311f5a596d
Hitomi <hitomi4vv@outlook.jp>
2018-02-14 06:25:16 +0900

    Add .rs source code file of Sorting Three Numbers

--- introduction/ITP1_2_C-Sorting Three Numbers.rs
+++ introduction/ITP1_2_C-Sorting Three Numbers.rs
@@ -2,6 +2,7 @@ use std::io;
 fn main() {
   let mut input = String::new();
   io::stdin().read_line(&mut input).unwrap();
-  let abc:Vec<i32> = input.trim().split(" ").map(|s| s.parse().unwrap()).collect();
-  println!("{:?}", abc);
+  let mut abc:Vec<_> = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+  abc.sort();
+  println!("{} {} {}", abc[0], abc[1], abc[2]);
 }
