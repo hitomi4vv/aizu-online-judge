3f7c579625b68794b651beb6804688565424f71d
Hitomi <hitomi4vv@outlook.jp>
2018-02-16 23:48:46 +0900

    wip

--- introduction/ITP1_2_D-Circle in a Rectangle.go
+++ introduction/ITP1_2_D-Circle in a Rectangle.go
@@ -9,5 +9,6 @@ func main() {
   sc := bufio.NewScanner(os.Stdin)
   sc.Scan()
   line := strings.Split(sc.Text(), " ")
-  fmt.Println(line)
+  w, h, x, y, r := line[0], line[1], line[2], line[3], line[4]
+  fmt.Println(w, h, x, y, r)
 }
