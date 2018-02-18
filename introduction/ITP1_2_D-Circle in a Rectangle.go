39c37bb5c0487df02324aec52867a5b8429f5094
Hitomi <hitomi4vv@outlook.jp>
2018-02-18 18:55:18 +0900

    Add .go source code file of Circle in a Rectangle

--- introduction/ITP1_2_D-Circle in a Rectangle.go
+++ introduction/ITP1_2_D-Circle in a Rectangle.go
@@ -1,15 +1,12 @@
 package main
-import (
-  "bufio"
-  "fmt"
-  "os"
-  "strings"
-)
+import "fmt"
 func main() {
-  sc := bufio.NewScanner(os.Stdin)
-  sc.Scan()
-  line := strings.Split(sc.Text(), " ")
-  w, h, x, y, r := line[0], line[1], line[2], line[3], line[4]
+  var w, h, x, y, r int
+  fmt.Scan(&w)
+  fmt.Scan(&h)
+  fmt.Scan(&x)
+  fmt.Scan(&y)
+  fmt.Scan(&r)
   if r <= x && x <= w - r && r <= y && y <= h - r {
     fmt.Println("Yes")
   } else {
