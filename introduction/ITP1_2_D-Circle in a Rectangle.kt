866c4f36252de221244ebfea770df0c02dd48d0a
Hitomi <hitomi4vv@outlook.jp>
2018-02-20 06:03:53 +0900

    wip

--- introduction/ITP1_2_D-Circle in a Rectangle.kt
+++ introduction/ITP1_2_D-Circle in a Rectangle.kt
@@ -1,4 +1,9 @@
 fun main(args:Array<String>) {
   val line = readLine()!!.split(" ").map{it.toInt()}.toTypedArray()
-  println(line)
+  val w = line[0]
+  val h = line[1]
+  val x = line[2]
+  val y = line[3]
+  val r = line[4]
+  println(w)
 }
