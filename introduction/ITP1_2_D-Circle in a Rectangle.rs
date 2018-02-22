71f338d9cc2f243708e4c6ac505875e6c6e99b24
Hitomi <hitomi4vv@outlook.jp>
2018-02-23 00:19:54 +0900

    wip

--- introduction/ITP1_2_D-Circle in a Rectangle.rs
+++ introduction/ITP1_2_D-Circle in a Rectangle.rs
@@ -3,5 +3,6 @@ fn main() {
   let mut input = String::new();
   io::stdin().read_line(&mut input).unwrap();
   let mut line:Vec<_> = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
-  println!("{}", line[0]);
+  let (w, h, x, y, r) = (line[0], line[1], line[2], line[3], line[4]);
+  println!("{} {} {} {} {}", w, h, x, y, r);
 }
