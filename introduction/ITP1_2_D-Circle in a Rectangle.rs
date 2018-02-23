380434af4fce95b3ade414c76c665266a555617f
Hitomi <hitomi4vv@outlook.jp>
2018-02-24 00:21:54 +0900

    Add .rs source code file of Circle in a Rectangle

--- introduction/ITP1_2_D-Circle in a Rectangle.rs
+++ introduction/ITP1_2_D-Circle in a Rectangle.rs
@@ -4,5 +4,9 @@ fn main() {
   io::stdin().read_line(&mut input).unwrap();
   let mut line:Vec<_> = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
   let (w, h, x, y, r) = (line[0], line[1], line[2], line[3], line[4]);
-  println!("{} {} {} {} {}", w, h, x, y, r);
+  if r <= x && x <= w - r && r <= y && y <= h - r {
+    println!("Yes");
+  } else {
+    println!("No");
+  }
 }
