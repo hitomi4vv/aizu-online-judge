6b56d9bb19e02e8482ca1b3ad57d85304e14ec14
Hitomi <hitomi4vv@outlook.jp>
2018-03-02 00:56:22 +0900

    LINQ only

--- introduction/ITP1_2_C-Sorting Three Numbers.cs
+++ introduction/ITP1_2_C-Sorting Three Numbers.cs
@@ -1,13 +1,7 @@
 using System;
 using System.Linq;
-
 public class Program {
   public static void Main() {
-    var line = Console.ReadLine().Split();
-
-    int[] arr = line.Select(int.Parse).ToArray();
-    Array.Sort(arr);
-
-    Console.WriteLine(string.Join(" ", arr.Select(x => x.ToString()).ToArray()));
+    Console.WriteLine(Console.ReadLine().Split().OrderBy(int.Parse).Aggregate((x,y) => x+" "+y));
   }
 }
