bae22f3094eece7e1bcaf71e0d281ef667cc6f3e
Hitomi <hitomi4vv@outlook.jp>
2018-03-03 03:08:03 +0900

    Add .kt source code file of Print Test Cases

--- introduction/ITP1_3_B-Print Test Cases.kt
+++ introduction/ITP1_3_B-Print Test Cases.kt
@@ -1,6 +1,9 @@
 fun main(args:Array<String>) {
   var count = 1
   while (true) {
+    val line = readLine()!!.toInt()
+    if (line == 0) break
+    println("Case $count: $line")
     count++
   }
 }
