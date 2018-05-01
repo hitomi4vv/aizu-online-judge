306a1f83003fa013475c8e7450f9bc1d251a711a
Hitomi <hitomi4vv@outlook.jp>
2018-05-02 06:54:57 +0900

    wip

--- introduction/ITP1_3_C-Swapping Two Numbers.kt
+++ introduction/ITP1_3_C-Swapping Two Numbers.kt
@@ -2,7 +2,6 @@ fun main(args:Array<String>) {
   while (true) {
     val line = readLine()!!
     if (line == "0 0") break
-    val l = line.split(" ")
-    println(l)
+    println(line.split(" ").reversed())
   }
 }
