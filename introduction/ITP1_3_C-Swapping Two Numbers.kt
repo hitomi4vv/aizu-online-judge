658dc1d5357c1b6b7f6f78a700158653fccc4959
Hitomi <hitomi4vv@outlook.jp>
2018-05-03 00:37:20 +0900

    Judge was 'Wrong Answer' in TestCase1

--- introduction/ITP1_3_C-Swapping Two Numbers.kt
+++ introduction/ITP1_3_C-Swapping Two Numbers.kt
@@ -2,6 +2,6 @@ fun main(args:Array<String>) {
   while (true) {
     val line = readLine()!!
     if (line == "0 0") break
-    println(line.split(" ").reversed())
+    println(line.split(" ").reversed().joinToString(separator = " "))
   }
 }
