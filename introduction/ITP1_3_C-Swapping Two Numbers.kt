a956ae4266dc53b4c7b2c0fe694c602c07ca923a
Hitomi <hitomi4vv@outlook.jp>
2018-05-05 00:01:20 +0900

    Add .kt source code file of Swapping Two Numbers

--- introduction/ITP1_3_C-Swapping Two Numbers.kt
+++ introduction/ITP1_3_C-Swapping Two Numbers.kt
@@ -2,6 +2,6 @@ fun main(args:Array<String>) {
   while (true) {
     val line = readLine()!!
     if (line == "0 0") break
-    println(line.split(" ").sorted().joinToString(separator = " "))
+    println(line.split(" ").map{it.toInt()}.sorted().joinToString(separator = " "))
   }
 }
