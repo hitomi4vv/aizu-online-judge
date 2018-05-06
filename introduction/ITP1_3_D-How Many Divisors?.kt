78d997f5545f44def9cfb6b9deedbdfd2683dd42
Hitomi <hitomi4vv@outlook.jp>
2018-05-07 00:03:33 +0900

    wip

--- introduction/ITP1_3_D-How Many Divisors?.kt
+++ introduction/ITP1_3_D-How Many Divisors?.kt
@@ -1,8 +1,5 @@
 fun main(args:Array<String>) {
-  val line = readLine()!!.split(" ").map{it.toInt()}.toTypedArray()
-  val a = line[0]
-  val b = line[1]
-  val c = line[2]
+  val line = readLine()!!.split(" ").map{it.toInt()}
   val n = 0
   println(n)
 }
