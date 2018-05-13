31ab4cfb3f34736cc56c4aea537a2bd00d7e6fd6
Hitomi <hitomi4vv@outlook.jp>
2018-05-13 23:11:24 +0900

    Add .rs source code file of Swapping Two Numbers

--- introduction/ITP1_3_C-Swapping Two Numbers.rs
+++ introduction/ITP1_3_C-Swapping Two Numbers.rs
@@ -4,6 +4,7 @@ fn main() {
   for line in stdin.lock().lines() {
     let mut l: Vec<_> = line.unwrap().split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
     if l == [0, 0] { break; }
+    l.sort();
     println!("{} {}", l[0], l[1]);
   }
 }
