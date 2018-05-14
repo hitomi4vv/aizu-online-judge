b5adca6985601aa1ea60968313607b1c40931317
Hitomi <hitomi4vv@outlook.jp>
2018-05-15 08:37:26 +0900

    wip

--- introduction/ITP1_3_D-How Many Divisors?.rs
+++ introduction/ITP1_3_D-How Many Divisors?.rs
@@ -2,7 +2,7 @@ use std::io;
 fn main() {
   let mut input = String::new();
   io::stdin().read_line(&mut input).unwrap();
-  let mut line:Vec<_> = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
-  let (a, b, c) = (line[0], line[1], line[2]);
-  println!("{} {} {}", a, b, c);
+  let l:Vec<_> = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+  let n = (l[0]..(l[1] + 1)).filter(|i| l[2] % i == 0).count();
+  println!("{}", n);
 }
