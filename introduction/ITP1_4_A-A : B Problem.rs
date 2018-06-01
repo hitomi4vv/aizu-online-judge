3b0780dd20f8148b9cddca1d2410d376af3f6d85
Hitomi <hitomi4vv@outlook.jp>
2018-06-02 06:09:51 +0900

    wip

---
+++ introduction/ITP1_4_A-A : B Problem.rs
@@ -0,0 +1,8 @@
+use std::io;
+fn main() {
+  let mut input = String::new();
+  io::stdin().read_line(&mut input).unwrap();
+  let l:Vec<_> = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+  let (a, b) = (l[0], l[1]);
+  println!("a:{}, b:{}", a, b);
+}
