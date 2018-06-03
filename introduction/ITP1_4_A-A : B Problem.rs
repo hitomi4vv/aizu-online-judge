eb82d11eff800e7f0a89bf4d4988342e41c2595c
Hitomi <hitomi4vv@outlook.jp>
2018-06-04 00:27:35 +0900

    Add .rs source code file of A : B Problem

--- introduction/ITP1_4_A-A : B Problem.rs
+++ introduction/ITP1_4_A-A : B Problem.rs
@@ -4,5 +4,5 @@ fn main() {
   io::stdin().read_line(&mut input).unwrap();
   let l:Vec<_> = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
   let (a, b) = (l[0], l[1]);
-  println!("{} {} {}", a/b, a%b, a/b);
+  println!("{} {} {}", a/b, a%b, a as f64 / b as f64);
 }
