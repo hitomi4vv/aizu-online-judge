a07989413b4ba57adc8ed42df4b0712567e97ff7
Hitomi <hitomi4vv@outlook.jp>
2018-06-06 17:43:01 +0900

    wip

--- introduction/ITP1_4_B-Circle.rs
+++ introduction/ITP1_4_B-Circle.rs
@@ -3,5 +3,5 @@ fn main() {
   let mut input = String::new();
   io::stdin().read_line(&mut input).unwrap();
   let r:f64 = input.trim().parse().unwrap();
-  println!("{}", r);
+  println!("{} {}", r*r*PI, 2*PI*r);
 }
