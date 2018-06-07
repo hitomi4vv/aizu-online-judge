0ba585e188a6a2935bf53fe76598f7fa5beb53de
Hitomi <hitomi4vv@outlook.jp>
2018-06-07 15:02:30 +0900

    Add .rs source code file of Circle

--- introduction/ITP1_4_B-Circle.rs
+++ introduction/ITP1_4_B-Circle.rs
@@ -1,7 +1,8 @@
 use std::io;
+use std::f64::consts::PI;
 fn main() {
   let mut input = String::new();
   io::stdin().read_line(&mut input).unwrap();
   let r:f64 = input.trim().parse().unwrap();
-  println!("{} {}", r*r*PI, 2*PI*r);
+  println!("{} {}", r*r*PI, 2.0*PI*r);
 }
