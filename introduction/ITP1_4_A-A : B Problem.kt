588bbbeb237fba6b451b5eaba60bcb33ef385dff
Hitomi <hitomi4vv@outlook.jp>
2018-07-15 07:16:27 +0900

    Judge was 'Wrong Answer' in TestCase7

--- introduction/ITP1_4_A-A : B Problem.kt
+++ introduction/ITP1_4_A-A : B Problem.kt
@@ -2,5 +2,5 @@ fun main(args:Array<String>) {
   val line = readLine()!!.split(" ").map{it.toInt()}.toTypedArray()
   val a = line[0]
   val b = line[1]
-  println("${a/b} ${a%b} ${a/b}");
+  println("${a/b} ${a%b} ${a.toDouble()/b.toDouble()}");
 }
