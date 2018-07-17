87c908958ebd44726c4a92eceb65390cd0d639dc
Hitomi <hitomi4vv@outlook.jp>
2018-07-17 21:55:11 +0900

    Add .kt source code file of A : B Problem

--- introduction/ITP1_4_A-A : B Problem.kt
+++ introduction/ITP1_4_A-A : B Problem.kt
@@ -1,6 +1,4 @@
 fun main(args:Array<String>) {
-  val line = readLine()!!.split(" ").map{it.toInt()}.toTypedArray()
-  val a = line[0]
-  val b = line[1]
-  println("%d %d %5f", a/b, a%b, a.toDouble()/b.toDouble());
+  val (a, b) = readLine()!!.split(" ").map{it.toInt()}
+  println("%d %d %5f".format(a/b, a%b, a.toDouble()/b.toDouble()))
 }
